84d827726434269235b1049524ea3b78787f5369
zotacy <tacyzacharyo@johndeere.com>
2020-10-19 11:37:45 -0400

    Moved body of server.js to fruitsController.js

--- controllers/fruitsController.js
+++ controllers/fruitsController.js
@@ -11,6 +11,17 @@ router.get('/', (req,res)=>{
     });
 });
 
+//GET ==> show form to user
+router.get('/new', (req,res)=>{
+    res.render('new.ejs');
+})
+//GET ==> get single object
+router.get('/:index',(req,res)=>{
+    res.render('show.ejs',{
+        fruit: fruits[req.params.index]
+    });
+});
+
 //POST ==> Create a new fruit
 router.post('/', (req,res)=> {
     // if the user click on the check button in the new.ejs then set true
@@ -23,5 +34,35 @@ router.post('/', (req,res)=> {
     fruits.push(req.body);
     res.redirect('/fruits') //redirects back to homepage & re-renders page
 });
+
+//DELETE ==> delete single object (Similar to get /fruits/:index on line 25)
+router.delete('/:index', (req,res)=>{
+    fruits.splice(req.params.index, 1)
+    res.redirect('/fruits')
+});
+
+//EDIT ==>
+//GET ==> prefill the data from the model and show form to user
+router.get('/:index/edit', function(req, res){
+	res.render(
+		'edit.ejs', //render views/edit.ejs
+		{ //pass in an object that contains
+			fruit: fruits[req.params.index], //the fruit object
+			index: req.params.index //... and its index in the array
+		}
+	);
+});
+
+//PUT ==> update the data in our model
+router.put('/:index', (req, res) => { //:index is the index of our fruits array that we want to change
+    if(req.body.readyToEat === 'on'){ //if checked, req.body.readyToEat is set to 'on'
+        req.body.readyToEat = true;
+    } else { //if not checked, req.body.readyToEat is undefined
+        req.body.readyToEat = false;
+    }
+	fruits[req.params.index] = req.body; //in our fruits array, find the index that is specified in the url (:index).  Set that element to the value of req.body (the input data)
+	res.redirect('/fruits'); //redirect to the index page
+});
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -14,45 +14,7 @@ app.use((req,res,next) => {
 app.use(express.urlencoded({extended:true}));
 app.use("/fruits", require("./controllers/fruitsController.js"));
 
-//GET ==> show form to user
-app.get('/fruits/new', (req,res)=>{
-    res.render('new.ejs');
-})
-//GET ==> get single object
-app.get('/fruits/:index',(req,res)=>{
-    res.render('show.ejs',{
-        fruit: fruits[req.params.index]
-    });
-});
-
-//DELETE ==> delete single object (Similar to get /fruits/:index on line 25)
-app.delete('/fruits/:index', (req,res)=>{
-    fruits.splice(req.params.index, 1)
-    res.redirect('/fruits')
-});
-
-//EDIT ==>
-//GET ==> prefill the data from the model and show form to user
-app.get('/fruits/:index/edit', function(req, res){
-	res.render(
-		'edit.ejs', //render views/edit.ejs
-		{ //pass in an object that contains
-			fruit: fruits[req.params.index], //the fruit object
-			index: req.params.index //... and its index in the array
-		}
-	);
-});
-
-//PUT ==> update the data in our model
-app.put('/fruits/:index', (req, res) => { //:index is the index of our fruits array that we want to change
-    if(req.body.readyToEat === 'on'){ //if checked, req.body.readyToEat is set to 'on'
-        req.body.readyToEat = true;
-    } else { //if not checked, req.body.readyToEat is undefined
-        req.body.readyToEat = false;
-    }
-	fruits[req.params.index] = req.body; //in our fruits array, find the index that is specified in the url (:index).  Set that element to the value of req.body (the input data)
-	res.redirect('/fruits'); //redirect to the index page
-});
+//Refactored body into fruitsController.js
 
 
 //LISTEN
