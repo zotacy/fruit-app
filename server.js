5617e02a5c5d366bf9ca62b84a912dd01349a64b
zotacy <tacyzacharyo@johndeere.com>
2020-10-19 12:29:13 -0400

    server.js refectored. public folder and css added

--- server.js
+++ server.js
@@ -3,19 +3,10 @@ const app = express();
 
 const methodOverride = require('method-override'); //Used to delete
 app.use(methodOverride('_method'));
-
-const fruits = require('./models/fruits.js');
-
-//middleware
-app.use((req,res,next) => {
-    console.log('I run for all routes')
-    next()
-});
+app.use(express.static("public"));
 app.use(express.urlencoded({extended:true}));
+
 app.use("/fruits", require("./controllers/fruitsController.js"));
-
-//Refactored body into fruitsController.js
-
 
 //LISTEN
 app.listen (3000, ()=>{
