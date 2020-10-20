9a351882cb9f9df6b3110bb222cbfc82f41685d1
zotacy <tacyzacharyo@johndeere.com>
2020-10-20 10:46:01 -0400

    create signup form. require POST and GET routes

---
+++ models/users.js
@@ -0,0 +1,18 @@
+const users = [
+    {
+        name:'Tony Stark',
+        username: 'ironman',
+        password: 'prettyawesome'
+    },
+    {
+        name:'Clark Kent',
+        username: 'superman',
+        password: `canfly`
+    },
+    {
+        name:'Bruce Wayne',
+        username: 'batman',
+        password: 'hasgadgets'
+    }
+]
+module.exports = users;
